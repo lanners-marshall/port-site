ca6f12de7e2238f03ba2490384c6a7b0e7e1be04
lanners-marshall <lanners.marshall@gmail.com>
2019-03-09 19:37:19 -0800

    update

--- src/App.js
+++ src/App.js
@@ -40,11 +40,18 @@ class App extends Component {
 	        <img className="img-fluid mb-5 d-block mx-auto" src="img/me.png" alt=""/>
 	        <h1 className="text-uppercase mb-0">Marshall Lanners</h1>
 	        <hr className="star-light"/>
-	        <h2 className="font-weight-light mb-0">Full Stack Web Developer</h2>
+	        <h2 className="font-weight-light mb-0">Full Stack Web Developer</h2><br />
+          <h2 className="font-weight-light mb-0">HTML- CSS - Javascript - React - Node - Python - C</h2>
+          <h2 className="font-weight-light mb-0">Firebase - Heroku - Netlify</h2>
 	      </div>
+        <div className="text-center mt-4">
+          <a className="btn btn-xl btn-outline-light" href="img/portfolio/resume.pdf" download>
+            <i className="fas fa-download mr-2"></i>
+            Download Resume
+          </a>
+        </div>
 	    </header>
 
-    
     <section className="portfolio" id="portfolio">
       <div className="container">
         <h2 className="text-center text-uppercase text-secondary mb-0">Portfolio</h2>
@@ -57,7 +64,7 @@ class App extends Component {
                   <i className="fas fa-search-plus fa-3x"></i>
                 </div>
               </div>
-              <img className="img-fluid" src="img/portfolio/dnd.png" alt=""/>
+              <img className="img-fluid" src="img/portfolio/tacos.png" alt=""/>
             </a>
           </div>
           <div className="col-md-6 col-lg-4">
@@ -67,11 +74,41 @@ class App extends Component {
                   <i className="fas fa-search-plus fa-3x"></i>
                 </div>
               </div>
-              <img className="img-fluid" src="img/portfolio/spotty.png" alt=""/>
+              <img className="img-fluid" src="img/portfolio/dnd.png" alt=""/>
             </a>
           </div>
           <div className="col-md-6 col-lg-4">
             <a className="portfolio-item d-block mx-auto" href="#portfolio-modal-3">
+              <div className="portfolio-item-caption d-flex position-absolute h-100 w-100">
+                <div className="portfolio-item-caption-content my-auto w-100 text-center text-white">
+                  <i className="fas fa-search-plus fa-3x"></i>
+                </div>
+              </div>
+              <img className="img-fluid" src="img/portfolio/spotty.png" alt=""/>
+            </a>
+          </div>
+          <div className="col-md-6 col-lg-4">
+            <a className="portfolio-item d-block mx-auto" href="#portfolio-modal-4">
+              <div className="portfolio-item-caption d-flex position-absolute h-100 w-100">
+                <div className="portfolio-item-caption-content my-auto w-100 text-center text-white">
+                  <i className="fas fa-search-plus fa-3x"></i>
+                </div>
+              </div>
+              <img className="img-fluid" src="img/portfolio/interface.png" alt=""/>
+            </a>
+          </div>
+          <div className="col-md-6 col-lg-4">
+            <a className="portfolio-item d-block mx-auto" href="#portfolio-modal-5">
+              <div className="portfolio-item-caption d-flex position-absolute h-100 w-100">
+                <div className="portfolio-item-caption-content my-auto w-100 text-center text-white">
+                  <i className="fas fa-search-plus fa-3x"></i>
+                </div>
+              </div>
+              <img className="img-fluid" src="img/portfolio/kitchen.png" alt=""/>
+            </a>
+          </div>
+          <div className="col-md-6 col-lg-4">
+            <a className="portfolio-item d-block mx-auto" href="#portfolio-modal-6">
               <div className="portfolio-item-caption d-flex position-absolute h-100 w-100">
                 <div className="portfolio-item-caption-content my-auto w-100 text-center text-white">
                   <i className="fas fa-search-plus fa-3x"></i>
@@ -84,25 +121,16 @@ class App extends Component {
       </div>
     </section>
 
-    
     <section className="bg-primary text-white mb-0" id="about">
       <div className="container">
-        <h2 className="text-center text-uppercase text-white">About</h2>
+        <h2 className="text-center text-uppercase text-white">About Me</h2>
         <hr className="star-light mb-5"/>
-        <div className="row">
-          <div className="col-lg-4 ml-auto">
-            <p className="lead">Hello I'm Marshall. I'm a full stack web developer. My strengths include JavaScript, React, Node, HTML, and CSS. I also really Love Python. I enjoy tickering around with new technologies and teaching myself new stacks.</p>
-          </div>
-          <div className="col-lg-4 mr-auto">
-            <p className="lead">I joined Lambda School, a full time 8 month programming bootcamp, for their full stack web development program. A full syllabus can be viewed<br/> <a href="https://lambdaschool.com/courses/cs/web/" style={custom}>- here -</a></p>
-          </div>
-        </div>
-        <div className="text-center mt-4">
-          <a className="btn btn-xl btn-outline-light" href="img/portfolio/resume.pdf" download>
-            <i className="fas fa-download mr-2"></i>
-            Download Resume
-          </a>
-        </div>
+          <p className="lead maxwidth">Along with coding in my spare time, I'm also a huge dance enthisuest and have been partner dancing for over 12 years and hold around 14 different dance awards. See me on Youtube <a className="whiteText" href="https://www.youtube.com/watch?v=cJA49oCV4o4">HERE</a>.</p>
+          <p className="lead maxwidth">
+          I also really love being outdoors and taking long hikes with friends.
+          See my latest adventures <a href="https://www.flickr.com/photos/168861944@N05/" className="whiteText">HERE</a> 
+          .</p>
+          <img className="img-fluid mb-5 d-block mx-auto awardsPic" src="img/portfolio/awards.jpg" alt=""/>
       </div>
     </section>
 
@@ -111,7 +139,7 @@ class App extends Component {
       <div className="container">
         <h2 className="text-center text-uppercase text-secondary mb-0">Contact Me</h2>
         <hr className="star-dark mb-5"/>
-	        <ReactContactForm to="lanners.marshall@mail.com" className="email-form" />
+	        <ReactContactForm to="lanners.marshall@gmail.com" className="email-form" />
       </div>
     </section>
     <footer className="footer text-center">
@@ -125,18 +153,8 @@ class App extends Component {
             <h4 className="text-uppercase mb-4">Around the Web</h4>
             <ul className="list-inline mb-0">
               <li className="list-inline-item">
-                <a className="btn btn-outline-light btn-social text-center rounded-circle" href="https://www.facebook.com/profile.php?id=100009554594412">
-                  <i className="fab fa-fw fa-facebook-f"></i>
-                </a>
-              </li>
-              <li className="list-inline-item">
-                <a className="btn btn-outline-light btn-social text-center rounded-circle" href="https://plus.google.com/100165334047738300644">
-                  <i className="fab fa-fw fa-google-plus-g"></i>
-                </a>
-              </li>
-              <li className="list-inline-item">
-                <a className="btn btn-outline-light btn-social text-center rounded-circle" href="https://twitter.com/LannersM206">
-                  <i className="fab fa-fw fa-twitter"></i>
+                <a className="btn btn-outline-light btn-social text-center rounded-circle" href="https://github.com/lanners-marshall">
+                  <i className="fab fa-fw fa-github"></i>
                 </a>
               </li>
               <li className="list-inline-item">
@@ -144,7 +162,6 @@ class App extends Component {
                   <i className="fab fa-fw fa-linkedin-in"></i>
                 </a>
               </li>
-              
             </ul>
           </div>
           <div className="col-md-4">
@@ -175,10 +192,10 @@ class App extends Component {
         <div className="container text-center">
           <div className="row">
             <div className="col-lg-8 mx-auto">
-              <h2 className="text-secondary text-uppercase mb-0">Encounter Generator</h2>
+              <h2 className="text-secondary text-uppercase mb-0">Lets Get Tacos</h2>
               <hr className="star-dark mb-5"/>
-              <img className="img-fluid mb-5" src="img/portfolio/dnd.png" alt=""/>
-              <p className="mb-5">5th edition dungeons and dragons Encounter Generator. This App will take was made with React. It takes input, filters, and generates the appropriate number of monsters given the party and difficulty. <a href="https://dungeon-helper.com/">Website</a>, <a href="https://github.com/lanners-marshall/dnd-encounter-generator"> Github</a>, <a href="https://trello.com/b/jaml8MNR/dndgenerator">Trello</a>, <a href="https://www.youtube.com/watch?v=XXcLGGRU6SQ&t=4s">Youtube</a></p>
+              <img className="img-fluid mb-5" src="img/portfolio/tacos.png" alt=""/>
+              <p className="mb-5">Event organizer App that lets you interact with the Yelp Api and the Google Api to look up your favorite locations to get tacos at, invite friends, and schedule meet-ups. <a href="https://taco-run-c453f.firebaseapp.com/">Website</a>, <a href="https://github.com/Lambda-School-Labs/labs10-taco-run"> Github</a>, <a href="https://trello.com/b/CaLMkz0e/labs10-taco-run">Trello</a>, </p>
               <a className="btn btn-primary btn-lg rounded-pill portfolio-modal-dismiss" href="#">
                 <i className="fa fa-close"></i>
                 Close Project</a>
@@ -196,11 +213,10 @@ class App extends Component {
         <div className="container text-center">
           <div className="row">
             <div className="col-lg-8 mx-auto">
-              <h2 className="text-secondary text-uppercase mb-0">Spotty</h2>
+              <h2 className="text-secondary text-uppercase mb-0">Encounter Generator</h2>
               <hr className="star-dark mb-5"/>
-              <img className="img-fluid mb-5" src="img/portfolio/spotty.png" alt=""/>
-              <p className="mb-5">This is a React App that interacts with the Spotify Api. It can be used to play music from your playlists, look up music and play it, build new tracks, and post those new tracks to your Spotify account. <a href="https://react-spotty.herokuapp.com/">Website</a>, <a href="https://github.com/lanners-marshall/spotty">Github</a>, <a href="https://www.youtube.com/watch?v=lkMOtWU6ptI&t=1s">Youtube</a></p>
-
+              <img className="img-fluid mb-5" src="img/portfolio/dnd.png" alt=""/>
+              <p className="mb-5">5th edition dungeons and dragons Encounter Generator. This App will take was made with React. It takes input, filters, and generates the appropriate number of monsters given the party and difficulty. <a href="https://dungeon-helper.com/">Website</a>, <a href="https://github.com/lanners-marshall/dnd-encounter-generator"> Github</a>, <a href="https://trello.com/b/jaml8MNR/dndgenerator">Trello</a>, <a href="https://www.youtube.com/watch?v=XXcLGGRU6SQ&t=4s">Youtube</a></p>
               <a className="btn btn-primary btn-lg rounded-pill portfolio-modal-dismiss" href="#">
                 <i className="fa fa-close"></i>
                 Close Project</a>
@@ -218,9 +234,70 @@ class App extends Component {
         <div className="container text-center">
           <div className="row">
             <div className="col-lg-8 mx-auto">
+              <h2 className="text-secondary text-uppercase mb-0">Spotty</h2>
+              <hr className="star-dark mb-5"/>
+              <img className="img-fluid mb-5" src="img/portfolio/spotty.png" alt=""/>
+              <p className="mb-5">This is a React App that interacts with the Spotify Api. It can be used to play music from your playlists, look up music and play it, build new tracks, and post those new tracks to your Spotify account. <a href="https://react-spotty.herokuapp.com/">Website</a>, <a href="https://github.com/lanners-marshall/spotty">Github</a>, <a href="https://www.youtube.com/watch?v=lkMOtWU6ptI&t=1s">Youtube</a></p>
+              <a className="btn btn-primary btn-lg rounded-pill portfolio-modal-dismiss" href="#">
+                <i className="fa fa-close"></i>
+                Close Project</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div className="portfolio-modal mfp-hide" id="portfolio-modal-4">
+      <div className="portfolio-modal-dialog bg-white">
+        <a className="close-button d-none d-md-block portfolio-modal-dismiss" href="#">
+          <i className="fa fa-3x fa-times"></i>
+        </a>
+        <div className="container text-center">
+          <div className="row">
+            <div className="col-lg-8 mx-auto">
+              <h2 className="text-secondary text-uppercase mb-0">SJ-Architechts</h2>
+              <hr className="star-dark mb-5"/>
+              <img className="custom-border img-fluid mb-5" src="img/portfolio/interface.png" alt=""/>
+              <p className="mb-5">This is a responsive user interface project I made for a fictional architectural company.<a href="https://user-interface-project.firebaseapp.com"> Website</a>, <a href="https://github.com/lanners-marshall/User-Interface-Project-Week">Github</a>, <a href="https://trello.com/b/QMU9X0Uf/sj-architechts-marshall-lanners">Trello</a></p>
+              <a className="btn btn-primary btn-lg rounded-pill portfolio-modal-dismiss" href="#">
+                <i className="fa fa-close"></i>
+                Close Project</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div className="portfolio-modal mfp-hide" id="portfolio-modal-5">
+      <div className="portfolio-modal-dialog bg-white">
+        <a className="close-button d-none d-md-block portfolio-modal-dismiss" href="#">
+          <i className="fa fa-3x fa-times"></i>
+        </a>
+        <div className="container text-center">
+          <div className="row">
+            <div className="col-lg-8 mx-auto">
+              <h2 className="text-secondary text-uppercase mb-0">Soup Kitchen</h2>
+              <hr className="star-dark mb-5"/>
+              <img className="custom-border img-fluid mb-5" src="img/portfolio/kitchen.png" alt=""/>
+              <p className="mb-5">I served as a scrum master for a group of 7 other Lambda Students. My job was to serve as a generalist developer who would jump in where needed, help debug code, lead stand-ups, keep the group on task with the project, and provide everyone with the resources needed to complete their soup kitchen application and advance in the Lambda school program.<a href="https://adoring-leakey-35b5c6.netlify.com"> Website</a>, <a href="https://github.com/team-soup">Github</a>, <a href="https://trello.com/b/fZEwk4EM/teamsoup">Trello</a>, <a href="https://docs.google.com/document/d/1UyglcK2ss2EE6JRjJRFYWH2sNqMGsxF97K_VqCHzLBU"> Google Document</a></p>
+              <a className="btn btn-primary btn-lg rounded-pill portfolio-modal-dismiss" href="#">
+                <i className="fa fa-close"></i>
+                Close Project</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div className="portfolio-modal mfp-hide" id="portfolio-modal-6">
+      <div className="portfolio-modal-dialog bg-white">
+        <a className="close-button d-none d-md-block portfolio-modal-dismiss" href="#">
+          <i className="fa fa-3x fa-times"></i>
+        </a>
+        <div className="container text-center">
+          <div className="row">
+            <div className="col-lg-8 mx-auto">
               <h2 className="text-secondary text-uppercase mb-0">Multi User Dungeon</h2>
               <hr className="star-dark mb-5"/>
-              <img className="img-fluid mb-5" src="img/portfolio/mud.png" alt=""/>
+              <img className="custom-border img-fluid mb-5" src="img/portfolio/mud.png" alt=""/>
               <p className="mb-5">I made a multi user dungeon in React and Django. It gives you the ability to interact with players in real time as you navigate your way through the dungeon.
               <a href="https://react-dungeon-89b7a.firebaseapp.com/mud"> Website</a>, <a href="https://github.com/lanners-marshall/LambdaMUD-Client">Github Client</a>, <a href="https://github.com/lanners-marshall/LambdaMUD-Project">Github Backend</a>, <a href="https://trello.com/b/yPJkKv9k/lambdamud-marshall-lanners">Trello</a>, <a href="https://www.youtube.com/watch?v=NbS8ljriXh4&t=265s">Youtube</a></p>
               <a className="btn btn-primary btn-lg rounded-pill portfolio-modal-dismiss" href="#">
